481b39a1be2846b2ab5894cc0d0739480f151636
zoltansera <dev.zoltan.sera@gmail.com>
2024-05-28 18:45:29 +0200

    Modify js

--- script/map.js
+++ script/map.js
@@ -1,40 +1,73 @@
-const options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0,
-};
+class GeoLoc {
 
-function geo() {
-    navigator.permissions.query({ name: 'geolocation' }).then(function(result) {
-        /* result.status = "prompt" */
-    });
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(success, error, options);
-    } else {
-        alert("Geolocation denied / not supported by browser.");
+    constructor() {
+        this.options = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+        };
+
+        this.lat = 47.488524;
+        this.long = 19.0421782;
+        this.acc = 0;
+
+        this.state = undefined;
+        this.map = L.map("map").setView([this.lat, this.long], 19);
+        this.askForGeolocationPermission();
+    }
+
+    askForGeolocationPermission = () => {
+        navigator.permissions.query({ name: "geolocation" }).then((result) => {
+            if (result.state === "granted") {
+                //showLocalNewsWithGeolocation();
+            } else if (result.state === "prompt") {
+                //showButtonToEnableLocalNews();
+                console.log("Enable later");
+            } else {
+                // Don't do anything if the permission was denied.
+            }
+            this.state = result.state;
+        });
+
+        if (navigator.geolocation) {
+            navigator.geolocation.watchPosition(this.success, this.error, this.options);
+            this.state = "watching";
+        } else {
+            alert("Not supported");
+        }
+    }
+
+    success = (pos) => {
+        this.lat = pos.coords.latitude;
+        this.long = pos.coords.longitude;
+        this.acc = pos.coords.accuracy;
+        console.log("Your current position is:");
+        console.log(`Latitude : ${this.lat}`);
+        console.log(`Longitude: ${this.long}`);
+        console.log(`More or less ${this.acc} meters.`);
+        this.showPosition(this.lat, this.long, this.acc);
+    }
+
+    error = (err) => {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    showPosition = () => {
+//        var map = L.map("map").setView([this.lat, this.long], 19);
+        this.map.setView([this.lat, this.long], 19);
+        L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            maxZoom: 21,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        }).addTo(this.map);
+    
+        var circle = L.circle([this.lat, this.long], {
+            color: "red",
+            fillColor: "#f03",
+            fillOpacity: 0.2,
+            radius: 2,
+        }).addTo(this.map);
     }
 }
 
-function showPosition(position) {
-    var map = L.map("map").setView([position.latitude, position.longitude], 13);
-    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    }).addTo(map);
-}
 
-function success(pos) {
-    const crd = pos.coords;
-    console.log("Your current position is:");
-    console.log(`Latitude : ${crd.latitude}`);
-    console.log(`Longitude: ${crd.longitude}`);
-    console.log(`More or less ${crd.accuracy} meters.`);
-
-    showPosition(crd);
-}
-
-function error(err) {
-    console.warn(`ERROR(${err.code}): ${err.message}`);
-}
-
-geo();
+const myLoc = new GeoLoc();
