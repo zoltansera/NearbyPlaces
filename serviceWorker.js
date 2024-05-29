5d2615eba8f4ef19ccdb7125378995a1318ebf2c
zoltansera <dev.zoltan.sera@gmail.com>
2024-05-29 08:20:54 +0200

    PWA test

---
+++ serviceWorker.js
@@ -0,0 +1,32 @@
+const staticSite = "nearbyplaces-site-v1";
+const assets = [
+    "/",
+    "/index.html",
+    "/style/style.css",
+    "/script/map.js"
+];
+
+self.addEventListener("install", (installEvent) => {
+    installEvent.waitUntil(
+        caches.open(staticSite).then((cache) => {
+            cache.addAll(assets);
+        })
+    );
+});
+
+self.addEventListener("fetch", (fetchEvent) => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then((res) => {
+            return res || fetch(fetchEvent.request);
+        })
+    );
+});
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+        navigator.serviceWorker
+            .register("/serviceWorker.js")
+            .then((res) => console.log("service worker registered"))
+            .catch((err) => console.log("service worker not registered", err));
+    });
+}
